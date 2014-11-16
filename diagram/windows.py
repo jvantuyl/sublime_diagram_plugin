e01919dcbb5c0d8e0d9bb86930f9acea219d6e88
lusitania <lusitania@users.noreply.github.com>
2014-11-16 18:04:30 +0100

    Fix indentation errors and pep8

--- diagram/windows.py
+++ diagram/windows.py
@@ -1,14 +1,15 @@
 from .base import BaseViewer
 import sys
 import os
-from subprocess import check_call, Popen as run_command
 
 
 class WindowsViewer(BaseViewer):
     def load(self):
-		if not sys.platform in ('win32',):
-			raise Exception("WindowsViewer only supported on Windows platforms")
-		
+        if sys.platform not in ('win32',):
+            raise Exception(
+                "WindowsViewer only supported on Windows platforms"
+            )
+
     def view(self, diagram_files):
-		for diagram_file in diagram_files:
-			os.startfile(diagram_file.name)
+        for diagram_file in diagram_files:
+            os.startfile(diagram_file.name)
