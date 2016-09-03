588c30cabb07279f8eb763228099b858000fdccd
leandro-costa <leandro.costa@ifba.edu.br>
2016-09-03 13:04:47 -0300

    focus group
    fix for layout cols 2 already seted

--- diagram/sublime3.py
+++ diagram/sublime3.py
@@ -9,4 +9,5 @@ class Sublime3Viewer(BaseViewer):
 	def view(self,diagram_files):
 		for diagram_file in diagram_files:
 			sublime.active_window().run_command("set_layout",{"cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0], "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]})
+			sublime.active_window().focus_group(1)
 			sublime.active_window().open_file(diagram_file.name)
